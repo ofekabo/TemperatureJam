f219daa1af81bf2014d881af346a67e1bcf0f945
Tomer <63203688+tomerc133@users.noreply.github.com>
2021-05-14 17:57:36 +0300

    new level in progress

--- Assets/Scripts/Components/Attacker.cs
+++ Assets/Scripts/Components/Attacker.cs
@@ -8,11 +8,15 @@ public class Attacker : MonoBehaviour
     [HideInInspector] public Transform player;
     public float delayBetweenAttacks = 1f;
     public float distanceToAttack = 2f;
+
     [Tooltip("Temperature changed applied to temp control based on ice/lava enemy type")]
     public int tempDamage = 5;
+
     [Tooltip("Damage applied to player health component")]
     public int healthDamage = 5;
+
     [SerializeField] float returnBackDelay = 0.2f;
+    [SerializeField] private float pushForce = 10f;
     private Rigidbody2D _rb;
     private BaseEnemy _enemy;
 
@@ -30,18 +34,19 @@ public class Attacker : MonoBehaviour
     {
         StartCoroutine("AttackCo");
     }
+
     [ContextMenu("Attack")]
     public IEnumerator AttackCo()
     {
         Vector2 originalPos = transform.position;
         Vector2 targetPos = player.position;
-      
+
         _enemy.animator.animator.enabled = false;
         _enemy.movement.DontMove();
         yield return new WaitForSeconds(0.1f);
-        _rb.AddForce((targetPos - originalPos).normalized * 15,ForceMode2D.Impulse);
+        _rb.AddForce((targetPos - originalPos).normalized * pushForce, ForceMode2D.Impulse);
         yield return new WaitForSeconds(returnBackDelay);
-        _rb.AddForce((originalPos - (Vector2)transform.position).normalized * 5,ForceMode2D.Impulse);
+        _rb.AddForce((originalPos - (Vector2) transform.position).normalized * 5, ForceMode2D.Impulse);
         StopCoroutine("AttackCo");
         _enemy.animator.animator.enabled = enabled;
         _enemy.movement.Move();
