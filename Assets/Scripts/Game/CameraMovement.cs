a79bc8a47beeeba134533181f1c7545f2b54c3dd
ofekabo <76842189+ofekabo@users.noreply.github.com>
2021-05-15 23:05:34 +0300

    can go back between rooms now

---
+++ Assets/Scripts/Game/CameraMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{    
+    
+    /// <summary>
+    /// Duplicate current doorway and place the previous room camera pos so it will work
+    /// </summary>
+    
+    [Tooltip("Camera Cooldown must be bigger than {tweenTime} by 0.15f")]
+    [SerializeField][Range(0.3f,1f)] float cameraCooldown = 0.8f;
+    [SerializeField][Range(0.1f,1f)] float tweenTime = 0.2f;
+    private float _cooldownInterval;
+    private void Start()
+    {
+        GameEvents.Current.OnDoorWayTriggerEnterCamera += MoveCameraToNextRoom;
+    }
+
+    private void Update()
+    {
+        _cooldownInterval += Time.deltaTime;
+    }
+
+    void  MoveCameraToNextRoom(Transform nextCamPos)
+    {
+        if (transform.position != new Vector3(nextCamPos.position.x,nextCamPos.position.y, -10) && _cooldownInterval > cameraCooldown)
+        {
+            LeanTween.move(gameObject,new Vector3 (nextCamPos.position.x,nextCamPos.position.y,-10),tweenTime);
+            _cooldownInterval = 0;
+        }
+           
+    }
+}
